5cf5ba7be9250c4a7e79fb8b8bba958f2248476b
JMcB17 <joel.mcbride1@live.com>
2017-05-18 15:37:05 +0100

    Add files via upload

--- darthplagueisbot/runbotc133refact.py
+++ darthplagueisbot/runbotc133refact.py
@@ -10,7 +10,7 @@ def findt(Text, Type):
     Trigger = 'Did you ever hear the tragedy of Darth Plagueis the wise'
     if Type == 0:
         return SequenceMatcher(None, Trigger, Text).ratio()
-    elif SequenceMatcher(None, Trigger, Text).ratio() > 0.73:
+    elif SequenceMatcher(None, Trigger, Text).ratio() > 0.8:
         return True
     else:
         return False
@@ -70,56 +70,56 @@ Tragedy = 'I thought not. It\'s not a story the Jedi would tell you. It\'s a Sit
 
 #run bot
 while True:
-    #start reading comment stream
-    for comment in subreddit.stream.comments():
-        try:
-            #increment 'comments checked' counter by 1
-            progress()
-            #ignore unknown unicode characters to avoid errors
-            CommentBody = comment.body.encode('ascii', 'replace')
-            #check for general match,
-            #check for essential terms,
-            #check comment has not been replied to already,
-            #check comment is not the wrong phrase
-            if (findt(CommentBody, 1) and
-                wordmatch(CommentBody.lower(), 'plagueis') and
-                wordmatch(CommentBody.lower(), 'tragedy') and
-                not str(comment) in Cache.get('actions') and
-                not SequenceMatcher(None, Tragedy, CommentBody).ratio() > 0.66):
-                
-                #newline
-                print('')
-                #display id, body, author and match percentage of comment
-                print(comment)
-                print(CommentBody)
-                print(comment.author)
-                print(findt(CommentBody, 0))
-                #reply to comment
-                comment.reply(Tragedy)
-                #add comment to list of comments that have been replied to
-                log(comment)
-                #newline
-                print('')
-        #countdown for new accounts with limited comments/minute
-        except praw.exceptions.APIException as err:
-            ErrorDetails = str(err)
-            #get time till you can comment again from error details
-            WaitTime = ErrorDetails[54:55]
-            print('Wait ' + WaitTime + ' minutes to work.')
-            Time = int(WaitTime)
-            #display time remaining every minute
-            for i in range(Time):
-                sleep(60)
-                Time -= 1
-                print(str(Time) + ' minute(s) left.')
-        #handler for error thrown when connection resets
-        except prawcore.exceptions.RequestException as err:
-            print(str(err))
-            print('Connection reset.')
-        #generic error handler
-        except:
-            print('ERROR')
-            #get error details and display them
-            e = sys.exc_info()
-            print(e)
-            
+    try:
+        #start reading comment stream
+        for comment in subreddit.stream.comments():
+            try:
+                #increment 'comments checked' counter by 1
+                progress()
+                #ignore unknown unicode characters to avoid errors
+                CommentBody = comment.body.encode('ascii', 'replace')
+                #check for general match,
+                #check for essential terms,
+                #check comment has not been replied to already,
+                #check comment is not the wrong phrase
+                if (findt(CommentBody, 1) and
+                    wordmatch(CommentBody.lower(), 'plagueis') and
+                    wordmatch(CommentBody.lower(), 'tragedy') and
+                    not str(comment) in Cache.get('actions') and
+                    not SequenceMatcher(None, Tragedy, CommentBody).ratio() > 0.66):
+                    
+                    #newline
+                    print('')
+                    #display id, body, author and match percentage of comment
+                    print(comment)
+                    print(CommentBody)
+                    print(comment.author)
+                    print(findt(CommentBody, 0))
+                    #reply to comment
+                    comment.reply(Tragedy)
+                    #add comment to list of comments that have been replied to
+                    log(comment)
+                    #newline
+                    print('')
+            #countdown for new accounts with limited comments/minute
+            except praw.exceptions.APIException as err:
+                ErrorDetails = str(err)
+                #get time till you can comment again from error details
+                WaitTime = ErrorDetails[54:55]
+                print('Wait ' + WaitTime + ' minutes to work.')
+                Time = int(WaitTime)
+                #display time remaining every minute
+                for i in range(Time):
+                    sleep(60)
+                    Time -= 1
+                    print(str(Time) + ' minute(s) left.')
+            #generic error handler
+            except:
+                print('ERROR')
+                #get error details and display them
+                e = sys.exc_info()
+                print(e)
+    #handler for error thrown when connection resets
+    except prawcore.exceptions.RequestException as err:
+        print(str(err))
+        print('Connection reset.')
